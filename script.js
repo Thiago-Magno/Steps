f49e64329cc698f32499703755c88403bbd2f6cc
Thiago-Magno <thiagomagnoomg@gmail.com>
2024-09-11 19:04:23 -0300

    finishe mini project Steps

---
+++ script.js
@@ -0,0 +1,53 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const steps = document.querySelectorAll('.step')
+
+let currentActive = 1
+
+next.addEventListener('click', () =>{
+    currentActive++
+
+    if (currentActive > steps.length) {
+        currentActive = steps.length
+    }
+    console.log(currentActive);
+    update()
+} )
+
+prev.addEventListener('click', () =>{
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+    console.log(currentActive);
+
+    update()
+    
+} )
+
+function update() {
+    steps.forEach((step, index)  => {
+        if (index < currentActive) {
+            step.classList.add('active')
+        }else{
+            step.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active');
+
+    progress.style.width = (actives.length -1 )/ (steps.length -1) * 100 + '%';
+    
+    if (currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === steps.length) {
+        next.disabled = true
+    }   else{
+
+        prev.disabled = false
+        next.disabled = false
+    }
+   
+}
